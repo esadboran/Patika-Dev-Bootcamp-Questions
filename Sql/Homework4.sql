fbe65deafc53d9e6cfa44c2bf6415341d8639ddd
Esad Boran <45075057+esadboran@users.noreply.github.com>
2022-12-15 17:58:28 +0300

    Create Homework4.sql

---
+++ Sql/Homework4.sql
@@ -0,0 +1,29 @@
+/*
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2.film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3.film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4.country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5.city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+*/
+
+
+SELECT DISTINCT replacement_cost 
+FROM film;
+
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film;
+
+SELECT COUNT(*)
+FROM film
+where  title like 'T%' and rating = 'G';
+
+
+SELECT COUNT(*) 
+FROM country 
+WHERE country like  '_____';
+
+
+SELECT COUNT(*) FROM city
+WHERE  city ILIKE '%r';
